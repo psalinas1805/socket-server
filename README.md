908e9e2fec9bd4c7ca17dc982fdcf8ea412ec788
unknown <pablo.salinas1805@gmail.com>
2019-02-22 10:30:56 -0300

    Primer commit -rest server

---
+++ README.md
@@ -0,0 +1,15 @@
+# Socket-Server
+
+Reconstruir modulos de Node
+```
+npm install
+```
+
+Generar el DIST
+tsc -w
+
+Levantar servidor, cualquiera de estos dos comandos
+```
+nodemon
+node dist/
+```
---
+++ index.ts
@@ -0,0 +1,22 @@
+import Server from './classes/server'
+import { SERVER_PORT } from "./global/environment";
+import router  from './routes/routes';
+import  cors from 'cors';
+import bodyParse from 'body-parser';
+
+const server = new Server();
+
+//BodyParser
+
+server.app.use(bodyParse.urlencoded({extended: true}) );
+server.app.use(bodyParse.json() );
+
+//CORS
+server.app.use(cors({origin: true, credentials: true }) );
+
+// Rutas de servicio
+server.app.use('/', router);
+
+server.start( ()=> {
+    console.log(`Servidor corriendo en puerto: ${SERVER_PORT}`);    
+})
